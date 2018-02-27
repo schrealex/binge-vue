fc200f5c29815e66b49e52161c49cfeea0ac1ea6
schrealex <cer34ll@gmail.com>
2018-02-27 23:43:38 +0100

    URL to retrieve eshop

--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -1,34 +1,43 @@
 <template>
   <ul>
-    <list-item v-for="item of items" v-bind:key="item.imdbID"><img :src="'http://image.tmdb.org/t/p/w342' + item.profile_path"></list-item>
+    <list-item v-for="item of items" v-bind:key="item">{{ item }}</list-item>
   </ul>
 </template>
 
 <script>
-import Vue from 'vue'
-import ListItem from './ListItem'
+import Vue from "vue";
+import ListItem from "./ListItem";
 
 export default {
-  components: {ListItem},
-  name: 'list-view',
-  data () {
+  components: { ListItem },
+  name: "list-view",
+  data() {
     return {
       items: []
-    }
+    };
   },
   methods: {
-    loadItems: function () {
-      Vue.http.get('https://api.themoviedb.org/3/search/person?api_key=f16bfeb0210b43f1f12d8d4ccc114ee9&query=Margot%20Robbie').then(response => {
-        console.log(response)
-        this.items = response.body.results
-        console.log(this.items.length)
-      })
+    loadItems: function() {
+      Vue.http
+        .get(
+          "http://search.nintendo-europe.com/nl/select?q=Mario%20Kart%208%20Deluxe&rows=5&start=0&wt=json&fq=type:GAME%20AND%20system_type:nintendoswitch*%20AND%20product_code_txt:*"
+        )
+        .then(
+          response => {
+            console.log(response);
+            this.items = ["ABC", "DEF"];
+          },
+          error => {
+            console.log("!");
+            console.log(error);
+          }
+        );
     }
   },
-  beforeMount () {
-    this.loadItems()
+  beforeMount() {
+    this.loadItems();
   }
-}
+};
 </script>
 
 <style scoped>
