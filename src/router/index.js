2201d25fed4254ee8efbdce3f98cc62384c59108
schrealex <cer34ll@gmail.com>
2018-02-28 12:47:13 +0100

    Bootstrap-vue added

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,15 @@
-<<<<<<< HEAD
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
-Vue.use(Router)
-=======
-import Vue from 'vue';
-import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "../components/HelloWorld";
 
 Vue.use(Router);
->>>>>>> Fun
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-<<<<<<< HEAD
+      path: "/",
+      name: "HelloWorld",
       component: HelloWorld
     }
   ]
-})
-=======
-      component: HelloWorld,
-    },
-  ],
 });
->>>>>>> Fun
