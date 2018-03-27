822b491a9168b819564c6171ed3285510debaa02
schrealex <cer34ll@gmail.com>
2018-03-27 16:24:36 +0200

    Get games using Nintendo Eshop

--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -6,6 +6,7 @@
 
 <script>
 import Vue from "vue";
+import SwitchEshop from "nintendo-switch-eshop";
 import ListItem from "./ListItem";
 
 export default {
@@ -18,21 +19,33 @@ export default {
   },
   methods: {
     loadItems: function() {
-      Vue.http
-        .get(
-          "https://search.nintendo-europe.com/nl/select?q=Mario%20Kart%208%20Deluxe&rows=5&start=0&wt=json&fq=type:GAME%20AND%20system_type:nintendoswitch*%20AND%20product_code_txt:*"
-        )
-        .then(
-          response => {
-            console.log("Response:");
-            console.log(response);
-            this.items = ["ABC", "DEF"];
-          },
-          error => {
-            console.log("!");
-            console.log(error);
-          }
-        );
+      console.log("loadItems");
+      SwitchEshop.getGamesEurope().then(
+        response => {
+          console.log("response");
+          console.log(response);
+        },
+        error => {
+          console.log("error");
+          console.log(error);
+        }
+      );
+
+      // Vue.http
+      //   .get(
+      //     "https://search.nintendo-europe.com/nl/select?q=Mario%20Kart%208%20Deluxe&rows=5&start=0&wt=json&fq=type:GAME%20AND%20system_type:nintendoswitch*%20AND%20product_code_txt:*"
+      //   )
+      //   .then(
+      //     response => {
+      //       console.log("Response:");
+      //       console.log(response);
+      //       this.items = ["ABC", "DEF"];
+      //     },
+      //     error => {
+      //       console.log("!");
+      //       console.log(error);
+      //     }
+      //   );
     }
   },
   beforeMount() {
--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import VueResource from 'vue-resource'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import VueResource from "vue-resource";
+import SwitchEshop from "nintendo-switch-eshop";
 
-Vue.use(VueResource)
+Vue.use(VueResource);
+Vue.use(SwitchEshop);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   render: h => h(App)
-})
+});
